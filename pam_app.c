04e2187592f8583e314f5c22ca372d284f0635f6
lizzzza <93871638+tpdox1@users.noreply.github.com>
2024-11-11 17:50:13 +0300

    Update pam_app.c

--- pam_app.c
+++ pam_app.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Функция для обработки сообщений PAM
 static int my_pam_conv(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr) {
     *resp = (struct pam_response *) malloc(sizeof(struct pam_response) * num_msg);
     if (*resp == NULL) {
@@ -13,7 +14,7 @@ static int my_pam_conv(int num_msg, const struct pam_message **msg, struct pam_r
         (*resp)[i].resp = NULL;
         (*resp)[i].resp_retcode = 0;
         if (msg[i]->msg_style == PAM_PROMPT_ECHO_OFF || msg[i]->msg_style == PAM_PROMPT_ECHO_ON) {
-            (*resp)[i].resp = strdup("test_password");  // Это стоит заменить на реальный ввод
+            (*resp)[i].resp = strdup("qwe");  // Пароль пользователя
         }
     }
     return PAM_SUCCESS;
@@ -22,13 +23,18 @@ static int my_pam_conv(int num_msg, const struct pam_message **msg, struct pam_r
 int main() {
     pam_handle_t *pamh = NULL;
     struct pam_conv conv = { my_pam_conv, NULL };
-    int retval = pam_start("my_pam_service", NULL, &conv, &pamh);
+    
+    const char *username = "elizaveta"; // Имя пользователя
+
+    // Запуск PAM с указанием имени сервиса и пользователя
+    int retval = pam_start("my_pam_service", username, &conv, &pamh);
 
     if (retval != PAM_SUCCESS) {
         fprintf(stderr, "pam_start failed: %s\n", pam_strerror(pamh, retval));
         return 1;
     }
 
+    // Попытка аутентификации
     retval = pam_authenticate(pamh, 0);
     if (retval != PAM_SUCCESS) {
         fprintf(stderr, "pam_authenticate failed: %s\n", pam_strerror(pamh, retval));
@@ -38,6 +44,7 @@ int main() {
 
     printf("Authenticated\n");
 
+    // Завершение работы с PAM
     pam_end(pamh, retval);
     return 0;
 }
