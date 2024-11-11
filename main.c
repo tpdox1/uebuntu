9487d8166f237565d3eadf560fd4a1def0cb0e5b
lizzzza <93871638+tpdox1@users.noreply.github.com>
2024-11-11 20:46:13 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-unsigned int checksum(const char *str);
+extern unsigned int enhanced_checksum(const char *str);
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Usage: %s <string>\n", argv[0]);
+        printf("Usage: ./app_enhanced <string>\n");
         return 1;
     }
-    unsigned int sum = checksum(argv[1]);
+
+    unsigned int sum = enhanced_checksum(argv[1]);
     printf("Checksum: %u\n", sum);
+
     return 0;
 }
