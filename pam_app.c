3970334d5a7a530953b155a4e691dbc92174a0f0
lizzzza <93871638+tpdox1@users.noreply.github.com>
2024-11-11 17:47:01 +0300

    Update pam_app.c

--- pam_app.c
+++ pam_app.c
@@ -1,19 +1,43 @@
 #include <security/pam_appl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+static int my_pam_conv(int num_msg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr) {
+    *resp = (struct pam_response *) malloc(sizeof(struct pam_response) * num_msg);
+    if (*resp == NULL) {
+        return PAM_BUF_ERR;
+    }
+
+    for (int i = 0; i < num_msg; ++i) {
+        (*resp)[i].resp = NULL;
+        (*resp)[i].resp_retcode = 0;
+        if (msg[i]->msg_style == PAM_PROMPT_ECHO_OFF || msg[i]->msg_style == PAM_PROMPT_ECHO_ON) {
+            (*resp)[i].resp = strdup("test_password");  // Это стоит заменить на реальный ввод
+        }
+    }
+    return PAM_SUCCESS;
+}
 
 int main() {
     pam_handle_t *pamh = NULL;
-    struct pam_conv conv = { NULL, NULL };
+    struct pam_conv conv = { my_pam_conv, NULL };
     int retval = pam_start("my_pam_service", NULL, &conv, &pamh);
-    if (retval == PAM_SUCCESS) {
-        retval = pam_authenticate(pamh, 0);
+
+    if (retval != PAM_SUCCESS) {
+        fprintf(stderr, "pam_start failed: %s\n", pam_strerror(pamh, retval));
+        return 1;
     }
-    if (retval == PAM_SUCCESS) {
-        printf("Authenticated\n");
-    } else {
-        printf("Authentication failed\n");
+
+    retval = pam_authenticate(pamh, 0);
+    if (retval != PAM_SUCCESS) {
+        fprintf(stderr, "pam_authenticate failed: %s\n", pam_strerror(pamh, retval));
+        pam_end(pamh, retval);
+        return 1;
     }
+
+    printf("Authenticated\n");
+
     pam_end(pamh, retval);
-    return retval == PAM_SUCCESS ? 0 : 1;
+    return 0;
 }
